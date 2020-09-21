0c72153556e35d52b787a2fd09f4e8691be76d72
moyashiharusamen <moyashi.harusamen@gmail.com>
2020-09-21 23:16:06 +0900

    グローバルコンポーネントの定義 | コンポーネントがやや複雑になった場合の例

--- 3/app.js
+++ 3/app.js
@@ -2,6 +2,37 @@ Vue.component('fruits-list-title', {
     template: '<h1>フルーツ一覧</h1>'
 })
 
+Vue.component('fruits-list-desc', {
+    template: '<p>季節の代表的なフルーツ一覧です</p>'
+})
+
+Vue.component('fruits-list-table', {
+    template: `
+    <table>
+        <tr>
+            <th>季節</th>
+            <th>フルーツ</th>
+        </tr>
+        <tr>
+            <td>春</td>
+            <td>いちご</td>
+        </tr>
+        <tr>
+            <td>夏</td>
+            <td>スイカ</td>
+        </tr>
+        <tr>
+            <td>秋</td>
+            <td>ぶどう</td>
+        </tr>
+        <tr>
+            <td>冬</td>
+            <td>みかん</td>
+        </tr>
+    </table>
+    `
+})
+
 new Vue({
     el: '#fruits-list'
 })
