e42afb55ea124c0f06188c205767076ad9d316c1
moyashiharusamen <moyashi.harusamen@gmail.com>
2020-09-22 15:11:00 +0900

    テンプレートを構築するその他の手段 | text/x-template

--- 3/app.js
+++ 3/app.js
@@ -1,8 +1,16 @@
+// new Vue({
+//     el: '#fruits-list',
+//     components: {
+//         'fruits-list-title': {
+//             template: '<h1>フルーツ一覧</h1>'
+//         }
+//     }
+// })
+
+Vue.component('fruits-list-title', {
+    template: '#fruits-list-title'
+})
+
 new Vue({
-    el: '#fruits-list',
-    components: {
-        'fruits-list-title': {
-            template: '<h1>フルーツ一覧</h1>'
-        }
-    }
+    el: '#fruits-list'
 })
