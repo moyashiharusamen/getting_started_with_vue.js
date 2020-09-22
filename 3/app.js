4709479953ab6d11635313dec5d13b03a9389483
moyashiharusamen <moyashi.harusamen@gmail.com>
2020-09-22 17:24:54 +0900

    コンポーネント間の通信 | 子コンポーネントから親コンポーネントへの通信

--- 3/app.js
+++ 3/app.js
@@ -1,1 +1,33 @@
-Vue.component()
+const counterButton = Vue.extend({
+    template: '<span>{{counter}}個 <button @click="addToCart">追加</button></span>',
+    data() {
+        return {
+            counter: 0
+        }
+    },
+    methods: {
+        addToCart() {
+            this.counter += 1;
+            this.$emit('increment'); // increment カスタムイベントの発火
+        }
+    }
+})
+
+new Vue({
+    el: '#fruits-counter',
+    components: {
+        'counter-button': counterButton
+    },
+    data: {
+        total: 0, // カート内の合計商品数
+        fruits: [
+            { name: '梨' },
+            { name: 'イチゴ' }
+        ]
+    },
+    methods: {
+        incrementCartStatus() {
+            this.total += 1;
+        }
+    }
+})
