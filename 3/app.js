dda6a574637d307d87e69c491939c7dd71d40678
moyashiharusamen <moyashi.harusamen@gmail.com>
2020-09-23 21:14:12 +0900

    コンポーネントの設計 | スロットコンテンツを生かしたヘッダーコンポーネントの作成 2

--- 3/app.js
+++ 3/app.js
@@ -1,11 +1,19 @@
 const headerTemplate = `
-    <div style="color:gray">
-        <slot name="header">※親から何も渡ってこない場合、この文が表示されます</slot>
+    <div>
+        <slot name="header">No title</slot>
+    </div>
+`;
+const contentTemplate = `
+    <div>
+        <slot name="content">No contents</slot>
     </div>
 `;
 
 Vue.component('page-header', {
     template: headerTemplate
+})
+Vue.component('page-content', {
+    template: contentTemplate
 })
 
 new Vue({
