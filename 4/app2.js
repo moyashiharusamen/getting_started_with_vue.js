6ef67bf65c3efcfcf1842043e32eb51849771b65
moyashiharusamen <moyashi.harusamen@gmail.com>
2020-09-26 16:41:31 +0900

    サンプルアプリケーションの実装 | API によるデータ通信

--- 4/app2.js
+++ 4/app2.js
@@ -1,5 +1,57 @@
+// JSON を返す関数
+// この関数を用いて擬似的に web API 経由で情報を取得したようにする
+const getUsers = (callback) => {
+    setTimeout(() => {
+        callback(null, [
+            {
+                id: 1,
+                name: 'Ishii Takuro'
+            },
+            {
+                id: 2,
+                name: 'Haru Toshio'
+            }
+        ])
+    }, 1000)
+};
+
 const UserList = {
-    template: '#user-list'
+    // HTML 上の script タグの id を指定する
+    template: '#user-list',
+    data() {
+        return {
+            loading: false,
+            users() { return {} }, // 初期の空配列
+            error: null
+        }
+    },
+
+    // 初期化時にデータを取得する
+    created() {
+        this.fetchData()
+    },
+
+    // $route の変更を watch することでルーティングが変更された時に再度データを取得
+    watch: {
+        '$route': 'fetchData'
+    },
+    methods: {
+        fetchData() {
+            this.loading = true;
+
+            // 取得したデータの結果を users に格納する
+            // Function.prototype.bind は this のスコープを渡すために利用
+            getUsers(((err, users) => {
+                this.loading = false;
+
+                if (err) {
+                    this.error = err.toString();
+                } else {
+                    this.users = users;
+                }
+            }).bind(this))
+        }
+    }
 };
 
 const router = new VueRouter({
