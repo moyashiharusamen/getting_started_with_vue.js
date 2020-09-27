50b8c965c051d2348c2c45608e56964ab71af605
moyashiharusamen <moyashi.harusamen@gmail.com>
2020-09-27 23:50:46 +0900

    サンプルアプリケーションの実装 | 詳細ページの実装

--- 4/app2.js
+++ 4/app2.js
@@ -1,18 +1,32 @@
+const userData = [
+    {
+        id: 1,
+        name: 'Ishii Takuro',
+        description: '遊撃手'
+    },
+    {
+        id: 2,
+        name: 'Haru Toshio',
+        description: '中堅手'
+    }
+]
+
 // JSON を返す関数
 // この関数を用いて擬似的に web API 経由で情報を取得したようにする
 const getUsers = (callback) => {
     setTimeout(() => {
-        callback(null, [
-            {
-                id: 1,
-                name: 'Ishii Takuro'
-            },
-            {
-                id: 2,
-                name: 'Haru Toshio'
-            }
-        ])
-    }, 1000)
+        callback(null, userData)
+    }, 1000);
+};
+
+const getUser = (userId, callback) => {
+    setTimeout(() => {
+        const filterdUsers = userData.filter((user) => {
+            return user.id === parseInt(userId, 10);
+        });
+
+        callback(null, filterdUsers && filterdUsers[0])
+    });
 };
 
 const UserList = {
@@ -54,6 +68,37 @@ const UserList = {
     }
 };
 
+const UserDetail = {
+    template: '#user-detail',
+    data() {
+        return {
+            loading: false,
+            user: null,
+            error: null
+        }
+    },
+
+    created() {
+        this.fetchData();
+    },
+
+    methods: {
+        fetchData() {
+            this.loading = true;
+
+            getUser(this.$route.params.userId, ((err, user) => {
+                this.loading = false;
+
+                if (err) {
+                    this.error = err.toString();
+                } else {
+                    this.user = user;
+                }
+            }).bind(this));
+        }
+    }
+}
+
 const router = new VueRouter({
     routes: [
         {
@@ -65,6 +110,10 @@ const router = new VueRouter({
         {
             path: '/users',
             component: UserList
+        },
+        { // ルート定義の追加
+            path: '/users/:userId',
+            component: UserDetail
         }
     ]
 });
