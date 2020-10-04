cc8cc1e909c287c89b64b858d046804393721c3b
moyashiharusamen <moyashi.harusamen@gmail.com>
2020-10-05 00:02:05 +0900

    スロット | 名前付きスロット

---
+++ 5/slot/2/app.js
@@ -0,0 +1,27 @@
+const MyPage = {
+    template: `
+        <div>
+            <header>
+                <!-- ヘッダーのスロット（名前付きスロット） -->
+                <slot name="header"></slot>
+            </header>
+
+            <main>
+                <!-- ボディのスロット -->
+                <slot></slot>
+            </main>
+
+            <footer>
+                <!-- フッターのスロット（名前付きスロット） -->
+                <slot name="footer"></slot>
+            </footer>
+        </div>
+    `
+};
+
+new Vue({
+    el: '#app',
+    components: {
+        MyPage: MyPage
+    }
+})
