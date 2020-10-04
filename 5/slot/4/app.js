015d5fe384ea46d139a4a6a066fa9b4b14cad3f1
moyashiharusamen <moyashi.harusamen@gmail.com>
2020-10-05 00:24:02 +0900

    スロット | スロットのスコープ、スロット付きスコープ

---
+++ 5/slot/4/app.js
@@ -0,0 +1,37 @@
+const TodoList = {
+    props: {
+        todos: {
+            type: Array,
+            required: true
+        }
+    },
+    template: `
+        <ul>
+            <template v-for="todo in todos">
+                <!-- v-bind ディレクティブで todo を親コンポーネントに渡す -->
+                <slot :todo="todo">
+                    <li :key="todo.id">
+                        {{ todo.text }}
+                    </li>
+                </slot>
+            </template>
+        </ul>
+    `
+};
+
+new Vue({
+    el: '#app',
+    data() {
+        return {
+            todos: [
+                { id: 1, text: 'C++', isCompleted: true },
+                { id: 2, text: 'JavaScript', isCompleted: false },
+                { id: 3, text: 'Java', isCompleted: true },
+                { id: 4, text: 'Perl', isCompleted: false }
+            ]
+        }
+    },
+    components: {
+        TodoList: TodoList
+    }
+})
