af1304d0ef01ef77313dc5ae46faffdb4923627a
moyashiharusamen <moyashi.harusamen@gmail.com>
2020-10-04 23:45:48 +0900

    スロット | 単一スロット

---
+++ 5/slot/1/app.js
@@ -0,0 +1,15 @@
+const MyButton = {
+    template: `
+        <button>
+            <!-- 親コンポーネントで渡されたコンテンツに差し替えられる -->
+            <slot>OK</slot>
+        </button>
+    `
+};
+
+new Vue({
+    el: '#app',
+    components: {
+        MyButton: MyButton
+    }
+})
