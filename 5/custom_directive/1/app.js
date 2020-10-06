dc0b1dc19928f8352ddb46fef231459dc637710f
moyashiharusamen <moyashi.harusamen@gmail.com>
2020-10-06 23:16:45 +0900

    カスタムディレクティブ | フック関数の引数

--- 5/custom_directive/1/app.js
+++ 5/custom_directive/1/app.js
@@ -1,12 +1,21 @@
 Vue.directive('fallback-image', {
-    bind(el) {
+    bind(el, binding) {
+        console.log('bind', binding);
+
         el.addEventListener('error', () => {
-            // 画像のロードに失敗したら実行される処理
             el.src = 'https://dummyimage.com/400x400/000/ffffff.png&text=no+image';
         });
+    },
+    update(el, binding) {
+        console.log('update', binding);
     }
 })
 
-new Vue({
-    el: '#app'
+const vm = new Vue({
+    el: '#app',
+    data() {
+        return {
+            altText: 'logo'
+        }
+    }
 })
