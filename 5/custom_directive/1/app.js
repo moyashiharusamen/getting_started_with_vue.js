5fdc68ef4d0b0aaa8a153361c3fc6c4559bcaf74
moyashiharusamen <moyashi.harusamen@gmail.com>
2020-10-08 23:26:13 +0900

    カスタムディレクティブ | image-fallback ディレクティブの機能追加

--- 5/custom_directive/1/app.js
+++ 5/custom_directive/1/app.js
@@ -2,12 +2,27 @@ Vue.directive('fallback-image', {
     bind(el, binding) {
         console.log('bind', binding);
 
-        el.addEventListener('error', () => {
-            el.src = 'https://dummyimage.com/400x400/000/ffffff.png&text=no+image';
+        const once = binding.modifiers.once;
+
+        el.addEventListener('error', function onError() {
+            // 値として指定された no image の URL を img 要素の src 属性値として設定する
+            el.src = binding.value;
+
+            // once 修飾子が指定されている場合は、イベントリスナーを削除する
+            if (once) {
+                el.removeEventListener('error', onError)
+            }
         });
     },
     update(el, binding) {
         console.log('update', binding);
+
+        if (
+            binding.oldValue !== binding.value &&
+            binding.oldValue === el.src
+        ) {
+            el.src = binding.value;
+        }
     }
 })
 
@@ -15,7 +30,8 @@ const vm = new Vue({
     el: '#app',
     data() {
         return {
-            altText: 'logo'
+            altText: 'logo',
+            noImageURL: 'https://dummyimage.com/400x400/000/ffffff.png&text=no+image'
         }
     }
 })
