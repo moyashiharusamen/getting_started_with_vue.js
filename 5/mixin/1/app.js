8e87affb7d11f85b1b17cf1dcc2ed6e97159e5e7
moyashiharusamen <moyashi.harusamen@gmail.com>
2020-11-18 23:26:43 +0900

    ミックスイン | ミックスインで機能を再利用する（ミックスインの定義）

--- 5/mixin/1/app.js
+++ 5/mixin/1/app.js
@@ -1,11 +1,12 @@
-const IconShareButton = {
-    template: `
-        <button @click="share"><i class="fas fa-share-square"></i></button>
-    `,
+// ミックスインの定義
+const Sharable = {
     data() {
         return {
             _isProcessing: false
         }
+    },
+    created() {
+        console.log('Sharable ミックスインのフックが呼ばれました')
     },
     methods: {
         share() {
@@ -25,30 +26,27 @@ const IconShareButton = {
     }
 }
 
+const IconShareButton = {
+    mixins: [Sharable],
+    created() {
+        console.log('IconShareButton のフックが呼ばれました')
+    },
+    template: `
+        <button @click="share"><i class="fas fa-share-square"></i></button>
+    `
+}
+
 const TextShareButton = {
+    mixins: [Sharable],
+    created() {
+        console.log('TextShareButton のフックが呼ばれました')
+    },
     template: `
         <button @click="share">{{ buttonLabel }}</button>
     `,
     data() {
         return {
-            buttonLabel: 'シェアする',
-            _isProcessing: false
-        }
-    },
-    methods: {
-        share() {
-            if (this._isProcessing) {
-                return
-            }
-            if (!window.confirm('シェアしますか？')) {
-                return
-            }
-            this._isProcessing = true
-            // 実際はここで SNS の SDK の API を呼び出す
-            setTimeout(() => {
-                window.alert('シェアしました')
-                this._isProcessing = false
-            }, 300)
+            buttonLabel: 'シェアする'
         }
     }
 }
