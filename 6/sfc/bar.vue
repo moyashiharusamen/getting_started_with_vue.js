cf9d8386870b0ee9cb9af9deab354a9ab3487827
moyashiharusamen <moyashi.harusamen@gmail.com>
2020-11-19 23:02:00 +0900

    単一ファイルコンポーネントによる開発 | スコープ付き CSS

---
+++ 6/sfc/bar.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="bar">
+    <h1 class="header">Bar コンポーネント</h1>
+    <p>これは Bar コンポーネントです。</p>
+    <!-- Foo コンポーネントに対して header クラスを指定 -->
+    <foo class="header" />
+  </div>
+</template>
+
+<script>
+import Foo from './foo'
+
+export default {
+  components: {
+    Foo
+  }
+}
+</script>
+
+<style scoped>
+.bar {
+  border: 1px solid red;
+  margin: 4px;
+  padding: 4px;
+}
+.header {
+  font-size: 125%;
+}
+</style>
---
+++ 6/sfc/root.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="root">
+    <h1 class="header">Root コンポーネント</h1>
+    <p>これは Root コンポーネントです。</p>
+    <foo />
+    <bar />
+  </div>
+</template>
+
+<script>
+import Foo from './foo'
+import Bar from './bar'
+
+export default {
+  components: {
+    Foo,
+    Bar
+  }
+}
+</script>
+
+<style>
+#root {
+  border: 1px solid blue;
+  margin: 4px;
+  padding: 4px;
+}
+.header {
+  font-size: 200%;
+}
+</style>
