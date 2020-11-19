f55b310859c3c9b253bf6596e3847c80f121d633
moyashiharusamen <moyashi.harusamen@gmail.com>
2020-11-19 23:13:04 +0900

    単一ファイルコンポーネントによる開発 | スコープ付き CSS のメリット

---
+++ 6/scoped_css_merit/root.vue
@@ -0,0 +1,34 @@
+<template>
+  <div id="root">
+    <h1 class="header">Rootコンポーネント</h1>
+    <p>これはRootコンポーネントでし。</p>
+    <foo />
+    <bar />
+  </div>
+</template>
+
+<script>
+import Foo from './foo'
+import Bar from './bar'
+
+export default {
+  components: {
+    Foo,
+    Bar
+  }
+}
+</script>
+
+<style>
+#root {
+  border: 1px solid blue;
+  margin: 4px;
+  padding: 4px;
+}
+.header {
+  font-size: 200%;
+}
+p {
+  text-decoration: underline;
+}
+</style>
